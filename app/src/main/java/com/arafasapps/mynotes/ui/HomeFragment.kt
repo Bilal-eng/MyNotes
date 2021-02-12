d9034a62ef7d7a2136f9db3089b1a7f96a3e4310
Bilal <bilal.khalil@arafas.com>
2021-02-13 00:45:39 +0300

    add Navigation component

---
+++ app/src/main/java/com/arafasapps/mynotes/ui/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.arafasapps.mynotes.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.arafasapps.mynotes.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_add.setOnClickListener {
+            view?.findNavController()?.navigate(HomeFragmentDirections.actionSaveNote())
+        }
+    }
+}
